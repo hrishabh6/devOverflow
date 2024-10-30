98b1e0fb7e1e916a125231893486b3ec130bccfe
hrishabh6 <hrishabhjoshi123@gmail.com>
2024-10-30 23:02:48 +0530

    bug fix 7

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -7,9 +7,9 @@ interface ThemeContextType {
   setMode: (mode: string) => void;
 }
 
-export const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+export const ThemeContext = createContext<any | undefined>(undefined);
 
-export function ThemeProvider({ children }: { children: React.ReactNode }): JSX.Element {
+export function ThemeProvider({ children } : { children: any})  {
   const [mode, setMode] = useState<string>('light');
 
   const handleThemeChange = () => {
