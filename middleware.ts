857adf9b8789b85d8ba64aaf04f5696137eddcee
hrishabh6 <hrishabhjoshi123@gmail.com>
2024-11-01 16:30:01 +0530

    Random Commit

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ export default clerkMiddleware(async (auth, req) => {
  if (isPublicRoute(req)) return // if it's a public route, do nothing
  await auth.protect() // for any other route, require auth
 })
-
+//s
 export const config = {
  matcher: [
  '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
