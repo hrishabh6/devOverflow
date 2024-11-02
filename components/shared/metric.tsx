fc616537ba20f6d83df0e6d3697b8f33d26e22f7
hrishabh6 <hrishabhjoshi123@gmail.com>
2024-11-02 14:15:57 +0530

    fixed ui

--- components/shared/metric.tsx
+++ components/shared/metric.tsx
@@ -25,22 +25,23 @@ const metric = ({
 
     const MetricContent = (
         <>
-            <Image src={imgUrl} width={16} height={16} alt={alt} className={`object-contain ${href ? 'rounded-full' : ''}`} />
             <p className={`flex items-center gap-1 ${textstyles}`}>
+                <Image src={imgUrl} width={16} height={16} alt={alt} className={`object-contain ${href ? 'rounded-full' : ''}`} />
                 {value}
                 <span className={`small-regular line-clamp-1 ${isAuthor ? 'max-sm:hidden' : ''}`}>
-                {title}
-               </span>
+                    {title}
+                </span>
             </p>
         </>
-    )       
+    )
 
     if (href) {
         return (
             <Link href={href}>
                 {MetricContent}
             </Link>
-        )}
+        )
+    }
 
     return (
         <div className='flex-center flex-wrap gap-1'>
